64469494cdb54cf9d0e40ec1a52ec63731bb0c3b
FARHAN TIRTA KESUMAH <tirta.farhn@gmail.com>
2024-11-25 22:34:51 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,9 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url): void
     {
-        if(env('APP_ENV') !== 'local')
-        {
-            $url->forceSchema('https');
-        }
+        URL::forceScheme('https');
     }
 }
